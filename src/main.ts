c6748f86f43cf86c6464c8c5aeaf4d44ec197190
Etana87 <etana87@gmail.com>
2024-10-12 13:15:04 +0200

    Ejercicio

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,35 @@
 import "./style.css";
 
-/*ESTILOS*/
-const estiloVar = "font-weigth: bold; font-size: 16px; color: green;"
+// ESTILOS
+const estiloNombre = "font-weight: bold; font-size: 16px; color: green;"
 
-console.log("%cHola", estiloVar);
+// GÉNEROS
+const popRock = "🎵 Pop Rock";
+const rock = "🎸 Rock";
+const hardRock = "🤘 Hard Rock";
+const clasica = "🎼 Clásica";
+
+
+// OBJETO Y VARIABLES
+interface Grupo {
+    nombre: string;
+    año: number;
+    activo: boolean;
+    genero: string;
+}
+
+const grupos: Grupo[] = [
+    { nombre: 'The Beatles', año: 1960, activo: true, genero: popRock },
+    { nombre: 'Queen', año: 1970, activo: false, genero: rock },
+    { nombre: 'AC DC', año: 1973, activo: true, genero: hardRock },
+    { nombre: 'Ludwig van Beethoven', año: 1770, activo: false, genero: clasica },
+    { nombre: 'The Rolling Stones', año: 1962, activo: true, genero: rock },
+];
+
+// MOSTRAR GRUPOS
+grupos.forEach(grupo => {
+    console.log(`%c${grupo.nombre}`, estiloNombre, 
+        `Año de fundación: ${grupo.año}, 
+        Activo: ${grupo.activo}, 
+        Género: ${grupo.genero}`);
+});
